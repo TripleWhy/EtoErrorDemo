c915c2c4df210392158fed0e49de27af3ce94b15
TripleWhy <TripleWhy@users.noreply.github.com>
2016-12-15 10:33:02 +0100

    Added error demo project

--- EtoErrorDemo/MainForm.cs
+++ EtoErrorDemo/MainForm.cs
@@ -4,54 +4,26 @@ using Eto.Drawing;
 
 namespace EtoErrorDemo
 {
+	// Executed on windows 10 with WPF and display scaling set to 125%.
 	public class MainForm : Form
 	{
 		public MainForm()
 		{
-			Title = "My Eto Form";
 			ClientSize = new Size(400, 350);
 
-			Content = new StackLayout
+			Content = new TabControl
 			{
-				Padding = 10,
-				Items =
+				Pages =
 				{
-					"Hello World!",
-                    // add more controls here
-                }
+					new TabPage
+					{
+						Content = new Scrollable
+						{
+							Border = BorderType.None
+						}
+					}
+				}
 			};
-
-			// create a few commands that can be used for the menu and toolbar
-			var clickMe = new Command { MenuText = "Click Me!", ToolBarText = "Click Me!" };
-			clickMe.Executed += (sender, e) => MessageBox.Show(this, "I was clicked!");
-
-			var quitCommand = new Command { MenuText = "Quit", Shortcut = Application.Instance.CommonModifier | Keys.Q };
-			quitCommand.Executed += (sender, e) => Application.Instance.Quit();
-
-			var aboutCommand = new Command { MenuText = "About..." };
-			aboutCommand.Executed += (sender, e) => MessageBox.Show(this, "About my app...");
-
-			// create menu
-			Menu = new MenuBar
-			{
-				Items =
-				{
-					// File submenu
-					new ButtonMenuItem { Text = "&File", Items = { clickMe } },
-					// new ButtonMenuItem { Text = "&Edit", Items = { /* commands/items */ } },
-					// new ButtonMenuItem { Text = "&View", Items = { /* commands/items */ } },
-				},
-				ApplicationItems =
-				{
-					// application (OS X) or file menu (others)
-					new ButtonMenuItem { Text = "&Preferences..." },
-				},
-				QuitItem = quitCommand,
-				AboutItem = aboutCommand
-			};
-
-			// create toolbar			
-			ToolBar = new ToolBar { Items = { clickMe } };
 		}
 	}
 }
